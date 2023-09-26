b072b09d4a9fdd7cb7a39c0f72f91d7e9ed60219
AlexAdutov <alexeyspark@yandex.ru>
2023-09-26 22:47:24 +0300

    2

--- src/js/__tests__/appTest.js
+++ src/js/__tests__/appTest.js
@@ -1,9 +1,9 @@
 import healthIndicator from '../app';
 
 test.each([
-  [{ name: 'char1', health: 90 }, 'healthy'],
-  [{ name: 'char2', health: 45 }, 'wounded'],
-  [{ name: 'char3', health: 5 }, 'critical'],
+  [{ name: 'маг1', health: 90 }, 'healthy'],
+  [{ name: 'маг2', health: 45 }, 'wounded'],
+  [{ name: 'маг3', health: 5 }, 'critical'],
 ])('testing health status for %p', (object, expected) => {
   const result = healthIndicator(object);
   expect(result).toBe(expected);
